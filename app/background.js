f311d3346d6d1552c527d7e3de20d64e5de202c3
Finn Thompson <nnifinn@gmail.com>
2019-05-30 15:54:48 -0700

    Rewrote app: sets YT's time since last activity regularly instead of waiting for pause dialog.

--- app/background.js
+++ app/background.js
@@ -1,8 +1,38 @@
+const TIME_BETWEEN_RESETS = 10 * 60 * 1000; // ten minutes
+const RESET_ACTIVITY = 'window.location = "javascript:window.yt.util.activity.setTimestamp(Date.now());";';
+
+let timers = {};
+
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (tab.active && tab.status === 'complete' && changeInfo.status === 'complete') {
-        let url = new URL(tab.url);
-        if (url.hostname.endsWith('youtube.com') && url.pathname.startsWith('/watch')) {
-            chrome.tabs.sendMessage(tabId, { reload: true, url });
+    let url = new URL(tab.url);
+    if (url.hostname.endsWith('youtube.com') && url.pathname.startsWith('/watch')) {
+        if (tab.status === 'complete' && changeInfo.status === 'complete') {
+            startKeepActive(tabId);
         }
+    } else {
+        endKeepActive(tabId);
     }
 });
+
+chrome.tabs.onRemoved.addListener((tabId, removeInfo) => endKeepActive(tabId));
+
+function startKeepActive(tabId) {
+    log('Attached to tab ' + tabId);
+    timers[tabId] = setInterval(() => {
+        chrome.tabs.executeScript(tabId, { code: RESET_ACTIVITY }, () => {
+            log('Reset time since active on tab ' + tabId);
+        });
+    }, TIME_BETWEEN_RESETS);
+}
+
+function endKeepActive(tabId) {
+    log('Detached from tab ' + tabId);
+    if (tabId in timers) {
+        clearInterval(timers[tabId]);
+    }
+}
+
+function log(message) {
+    let timestamp = new Date().toLocaleTimeString();
+    console.log('[' + timestamp + '] YOUTUBE-UNINTERRUPTED: ' + message.toString());
+}
